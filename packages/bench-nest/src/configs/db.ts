6d231839bbe7d4126c54286eb7fa5f8e5ba0f051
Przemyslaw Hadala (PGS Software) <phadala@pgs-soft.com>
2019-10-07 15:12:07 +0200

    Add CORS config.

--- packages/bench-nest/src/configs/db.ts
+++ packages/bench-nest/src/configs/db.ts
@@ -1,6 +1,8 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+
 import { User } from '../modules/users/models/user.entity'
 
-export const getDbConfig = (overrides?: object): object => ({
+export const getDbConfig = (overrides?: object): TypeOrmModuleOptions => ({
   type: 'postgres',
   host: 'host.docker.internal',
   port: 5432,
--- packages/bench-nest/src/main.ts
+++ packages/bench-nest/src/main.ts
@@ -3,6 +3,7 @@ import * as helmet from 'helmet'
 
 import { AppModule } from './app.module'
 import { ValidationPipe, INestApplication } from '@nestjs/common'
+import { corsConfig } from './configs/cors'
 
 export function applyMiddlewares(app: INestApplication): INestApplication {
   app.useGlobalPipes(new ValidationPipe({
@@ -15,6 +16,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: ['error', 'warn']})
   applyMiddlewares(app)
   app.use(helmet())
+  app.enableCors(corsConfig)
   app.listen(3000)
 }
 bootstrap()
